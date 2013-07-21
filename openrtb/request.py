218d353eb5f952d8d4d9ce5837983ef68439cfb4
Pavel Anossov <anossov@gmail.com>
2013-07-21 22:58:14 +0400

    add docstrings from spec

--- openrtb/request.py
+++ openrtb/request.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from decimal import Decimal
 
 from . import constants
@@ -5,6 +7,11 @@ from .base import Object, Array, Field
 
 
 class Publisher(Object):
+    u"""
+    The publisher object itself and all of its parameters are optional, so default values are not provided.
+    If an optional parameter is not specified, it should be considered unknown.
+    """
+
     id = Field(str)
     name = Field(str)
     cat = Field(Array(str))
@@ -12,6 +19,14 @@ class Publisher(Object):
 
 
 class Producer(Object):
+    u"""
+    The producer is useful when content where the ad is shown is syndicated,
+    and may appear on a completely different publisher.
+    The producer object itself and all of its parameters are optional, so default values are not provided.
+    If an optional parameter is not specified, it should be considered unknown.
+    This object is optional, but useful if the content producer is different from the site publisher.
+    """
+
     id = Field(str)
     name = Field(str)
     cat = Field(Array(str))
@@ -19,6 +34,16 @@ class Producer(Object):
 
 
 class Geo(Object):
+    u"""
+    The geo object itself and all of its parameters are optional, so default values are not provided.
+    If an optional parameter is not specified, it should be considered unknown.
+    Note that the Geo Object may appear in one or both the Device Object and the User Object.
+    This is intentional, since the information may be derived from either a device-oriented
+    source (such as IP geo lookup), or by user registration information (for example
+    provided to a publisher through a user registration).
+    If the information is in conflict, it’s up to the bidder to determine which information to use.
+    """
+
     lat = Field(float)
     lon = Field(float)
     country = Field(str)
@@ -35,18 +60,43 @@ class Geo(Object):
 
 
 class Segment(Object):
+    u"""
+    The data and segment objects together allow data about the user to be passed to bidders in the bid request.
+    Segment objects convey specific units of information from the provider identified in the parent data object.
+    The segment object itself and all of its parameters are optional, so default values are not provided;
+    if an optional parameter is not specified, it should be considered unknown.
+    """
+
     id = Field(str)
     name = Field(str)
     value = Field(str)
 
 
 class Data(Object):
+    u"""
+    The data and segment objects together allow data about the user to be passed to bidders in the bid request.
+    This data may be from multiple sources (e.g., the exchange itself, third party providers)
+    as specified by the data object ID field. A bid request can mix data objects from multiple providers.
+    The data object itself and all of its parameters are optional, so default values are not provided.
+    If an optional parameter is not specified, it should be considered unknown.
+    """
+
     id = Field(str)
     name = Field(str)
     segment = Field(Array(Segment))
 
 
 class User(Object):
+    u"""
+    The “user” object contains information known or derived about the human user of the device.
+    Note that the user ID is an exchange artifact (refer to the “device” object for hardware or platform derived IDs)
+    and may be subject to rotation policies.
+    However, this user ID must be stable long enough to serve reasonably as the basis for frequency capping.
+    The user object itself and all of its parameters are optional, so default values are not provided.
+    If an optional parameter is not specified, it should be considered unknown.
+    If device ID is used as a proxy for unique user ID, use the device object.
+    """
+
     id = Field(str)
     buyerid = Field(str)
     yob = Field(int)
@@ -58,6 +108,16 @@ class User(Object):
 
 
 class Device(Object):
+    u"""
+    The “device” object provides information pertaining to the device including
+    its hardware, platform, location, and carrier.
+    This device can refer to a mobile handset, a desktop computer, set top box or other digital device.
+    The device object itself and all of its parameters are optional, so default values are not provided.
+    If an optional parameter is not specified, it should be considered unknown.
+    In general, the most essential fields are either the IP address (to enable geo-lookup for the bidder),
+    or providing geo information directly in the geo object.
+    """
+
     dnt = Field(int)
     ua = Field(str)
     ip = Field(str)
@@ -83,6 +143,17 @@ class Device(Object):
 
 
 class Content(Object):
+    u"""
+    The content object itself and all of its parameters are optional, so default values are not provided.
+    If an optional parameter is not specified, it should be considered unknown.
+    This object describes the content in which the impression will appear (may be syndicated or non-syndicated content).
+    This object may be useful in the situation where syndicated content contains impressions
+    and does not necessarily match the publisher’s general content.
+    The exchange might or might not have knowledge of the page where the content is running,
+    as a result of the syndication method.
+    (For example, video impressions embedded in an iframe on an unknown web property or device.)
+    """
+
     id = Field(str)
     episode = Field(int)
     title = Field(str)
@@ -102,6 +173,14 @@ class Content(Object):
 
 
 class Site(Object):
+    u"""
+    A site object should be included if the ad supported content is part of a website (as opposed to an application).
+    A bid request must not contain both a site object and an app object.
+    The site object itself and all of its parameters are optional, so default values are not provided.
+    If an optional parameter is not specified, it should be considered unknown.
+    At a minimum, it’s useful to provide a page URL or a site ID, but this is not strictly required.
+    """
+
     id = Field(str)
     name = Field(str)
     domain = Field(str)
@@ -118,6 +197,15 @@ class Site(Object):
 
 
 class App(Object):
+    u"""
+    An “app” object should be included if the ad supported content is part of a mobile
+    application (as opposed to a mobile website).
+    A bid request must not contain both an “app” object and a “site” object.
+    The app object itself and all of its parameters are optional, so default values are not provided.
+    If an optional parameter is not specified, it should be considered unknown.
+    At a minimum, it’s useful to provide an App ID or bundle, but this is not strictly required.
+    """
+
     id = Field(str)
     name = Field(str)
     domain = Field(str)
@@ -134,6 +222,17 @@ class App(Object):
 
 
 class Banner(Object):
+    u"""
+    The “banner” object must be included directly in the impression object
+    if the impression offered for auction is display or rich media,
+    or it may be optionally embedded in the video object to describe
+    the companion banners available for the linear or non-linear video ad.
+    The banner object may include a unique identifier; this can be useful
+    if these IDs can be leveraged in the VAST response
+    to dictate placement of the companion creatives when multiple
+    companion ad opportunities of the same size are available on a page.
+    """
+
     w = Field(int)
     h = Field(int)
     id = Field(str)
@@ -154,6 +253,15 @@ class Banner(Object):
 
 
 class Video(Object):
+    u"""
+    The “video” object must be included directly in the impression object if the impression offered
+    for auction is an in-stream video ad opportunity.
+    Note that for the video object, many of the fields are non-essential for a minimally viable exchange interfaces.
+    These parameters do not necessarily need to be specified to the bidder,
+    if they are always the same for all impression,
+    or if the exchange chooses not to supply the additional information to the bidder.
+    """
+
     mimes = Field(Array(str), required=True)
     linearity = Field(constants.VideoLinearity, required=True)
     minduration = Field(int, required=True)
@@ -176,6 +284,13 @@ class Video(Object):
 
 
 class Impression(Object):
+    u"""
+    The “imp” object describes the ad position or impression being auctioned.
+    A single bid request can include multiple “imp” objects,
+    a use case for which might be an exchange that supports selling all ad positions on a given page as a bundle.
+    Each “imp” object has a required ID so that bids can reference them individually.
+    """
+
     id = Field(str, required=True)
     banner = Field(Banner)
     video = Field(Video)
@@ -189,6 +304,12 @@ class Impression(Object):
 
 
 class BidRequest(Object):
+    u"""
+    The top-level bid request object contains a globally unique bid request or auction ID.
+    This “id” attribute is required as is at least one “imp” (i.e., impression) object.
+    Other attributes are optional since an exchange may establish default values.
+    """
+
     id = Field(str, required=True)
     imp = Field(Array(Impression), required=True)
     site = Field(Site, default=Site())
--- openrtb/response.py
+++ openrtb/response.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from decimal import Decimal
 
 from . import constants
@@ -5,6 +7,22 @@ from .base import Object, Array, Field
 
 
 class Bid(Object):
+    u"""
+    For each bid, the “nurl” attribute contains the win notice URL.
+    If the bidder wins the impression, the exchange calls this notice URL
+        a) to inform the bidder of the win and
+        b) to convey certain information using substitution macros.
+
+    The “adomain” attribute can be used to check advertiser block list compliance.
+    The “iurl” attribute can provide a link to an image that is representative of the campaign’s
+    content (irrespective of whether the campaign may have multiple creatives).
+    This enables human review for spotting inappropriate content.
+    The “cid” attribute can be used to block ads that were previously identified as inappropriate;
+    essentially a safety net beyond the block lists.
+    The “crid” attribute can be helpful in reporting creative issues back to bidders.
+    Finally, the “attr” array indicates the creative attributes that describe the ad to be served.
+    """
+
     id = Field(str, required=True)
     impid = Field(str, required=True)
     price = Field(Decimal, required=True)
@@ -20,12 +38,31 @@ class Bid(Object):
 
 
 class SeatBid(Object):
+    u"""
+    A bid response can contain multiple “seatbid” objects, each on behalf of a different bidder seat.
+    Since a bid request can include multiple impressions,
+    each “seatbid” object can contain multiple bids each pertaining to a different impression on behalf of a seat.
+    Thus, each “bid” object must include the impression ID to which it pertains as well as the bid price.
+    The “group” attribute can be used to specify if a seat is willing to accept
+    any impressions that it can win (default) or if it is only interested
+    in winning any if it can win them all (i.e., all or nothing).
+    """
+
     bid = Field(Array(Bid), required=True)
     seat = Field(str)
     group = Field(int)
 
 
 class BidResponse(Object):
+    u"""
+    The top-level bid response object.
+    The “id” attribute is a reflection of the bid request ID for logging purposes.
+    Similarly, “bidid” is an optional response tracking ID for bidders.
+    If specified, it can be included in the subsequent win notice call if the bidder wins.
+    At least one “seatbid” object is required, which contains a bid on at least one impression.
+    Other attributes are optional since an exchange may establish default values.
+    """
+
     id = Field(str, required=True)
     seatbid = Field(Array(SeatBid), required=True)
     bidid = Field(str)
